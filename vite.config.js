d322f5ffa55bcf8b965a54d3690b7920bebbd672
sandeep <54143606+SandeepSINGH-ENG@users.noreply.github.com>
2024-08-08 18:15:11 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default {
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
   build: {
     rollupOptions: {
       external: ['vue-router']
     }
   }
-}
+})
